6e249570011dc196c69f212d88e3a45b060185d8
wasifsn <wasif4000.wn@gmail.com>
2020-05-24 19:49:28 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,92 @@
+const { app, BrowserWindow, Menu, globalShortcut } = require("electron");
+//set node env
+process.env.NODE_ENV = "development";
+
+const isDev = process.env.NODE_ENV !== "production" ? true : false;
+const isMac = process.platform === "darwin" ? true : false;
+
+let mainWindow;
+let aboutWindow;
+
+function creatAboutWindow() {
+  aboutWindow = new BrowserWindow({
+    width: 300,
+    height: 300,
+    title: "About ImageResize",
+    icon: "./assets/icons/Icon_256x256.png",
+    resizable: false,
+  });
+  aboutWindow.loadFile(`./app/about.html`);
+}
+
+function createMainWindow() {
+  mainWindow = new BrowserWindow({
+    width: 500,
+    height: 600,
+    title: "Image Resize",
+    icon: "./assets/icons/Icon_256x256.png",
+    resizable: isDev ? true : false,
+  });
+  // mainWindow.loadURL(`file://${__dirname}/app/index.html`);
+  mainWindow.loadFile(`./app/index.html`);
+}
+
+app.on("ready", () => {
+  createMainWindow();
+  const mainMenu = Menu.buildFromTemplate(menu);
+  Menu.setApplicationMenu(mainMenu);
+  globalShortcut.register(isMac ? "Command+Alt+i" : "Ctrl+Shift+i", () => {
+    mainWindow.toggleDevTools();
+  });
+  // globalShortcut.register("CmdorCtrl+R", () => {
+  //   mainWindow.reload();
+  // });
+  // mainWindow.on("ready", () => {
+  //   mainWindow = null;
+  // });
+});
+
+const menu = [
+  ...(isMac
+    ? [
+        {
+          label: app.name,
+          submenu: [{ label: "About", click: creatAboutWindow }],
+        },
+      ]
+    : []),
+  ...(!isMac
+    ? [
+        {
+          label: "Help",
+          submenu: [{ label: "About", click: creatAboutWindow }],
+        },
+      ]
+    : []),
+  { role: "fileMenu" },
+  ...(isDev
+    ? [
+        {
+          label: "Developer",
+          submenu: [
+            { role: "reload" },
+            { role: "forceReload" },
+            { type: "separator" },
+            { role: "toggledevtools" },
+          ],
+        },
+      ]
+    : []),
+];
+
+app.on("window-all-closed", () => {
+  if (!isMac) {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
